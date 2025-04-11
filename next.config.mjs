2057e432a4cd09ef641c2d0ad9d20620226ee369
Mahdi Hazrati <mahdihazratimailbox@gmail.com>
2025-04-11 07:54:28 +0330

    change

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,40 @@
-// @ts-check
- 
+let userConfig = undefined
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  images: {
+    unoptimized: true,
+  },
+  experimental: {
+    webpackBuildWorker: true,
+    parallelServerBuildTraces: true,
+    parallelServerCompiles: true,
+  },
 }
- 
-module.exports = nextConfig
+
+if (userConfig) {
+  // ESM imports will have a "default" property
+  const config = userConfig.default || userConfig
+
+  for (const key in config) {
+    if (
+      typeof nextConfig[key] === 'object' &&
+      !Array.isArray(nextConfig[key])
+    ) {
+      nextConfig[key] = {
+        ...nextConfig[key],
+        ...config[key],
+      }
+    } else {
+      nextConfig[key] = config[key]
+    }
+  }
+}
+
+export default nextConfig
